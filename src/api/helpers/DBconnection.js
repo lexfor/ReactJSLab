969e08d5492ec73a1658f38567145b713a61d8de
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-03 09:19:43 +0300

    fixes

--- src/api/helpers/DBconnection.js
+++ src/api/helpers/DBconnection.js
@@ -1,10 +1,12 @@
 import pkg from 'pg';
+import { DB_ACCESS } from '../../config';
 
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+  user: DB_ACCESS.user,
+  host: DB_ACCESS.host,
+  database: DB_ACCESS.database,
+  password: DB_ACCESS.password,
+  port: DB_ACCESS.port
+})
