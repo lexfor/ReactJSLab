8a98f910d24f48abcf410a1e1fa3549c365f2969
Tsimafei Kinevich <tsimafei.kinevich@itrexgroup.lan>
2021-11-12 13:14:15 +0300

    review fixes

--- src/api/helpers/conditions/dateCondition.js
+++ src/api/helpers/conditions/dateCondition.js
@@ -3,11 +3,13 @@
  * @param {string} date
  * @returns {string} date condition
  */
+import {ISOdateFixes} from "../ISOdateFixes";
+
 export function dateCondition(date) {
   let dateConditionFilter = '';
   if (date) {
-    dateConditionFilter = `AND (appointments.visit_date::text LIKE '%${date}%' 
-        OR resolutions.next_appointment_date::text LIKE '%${date}%')`;
+    dateConditionFilter = `AND (appointments.visit_date::text LIKE '%${ISOdateFixes(date)}%' 
+        OR resolutions.next_appointment_date::text LIKE '%${ISOdateFixes(date)}%')`;
   }
   return dateConditionFilter;
 }
